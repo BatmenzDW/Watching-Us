e8044ce54bf9c11dd73bbd7eebbd8fa5ca132ff5
paupertim <paupertim@gmail.com>
2026-01-25 17:55:16 -0600

    updated gen_path_3 to 3-3 relations
    Added the ability for 3-3 path relations to be generated with pathing system.
    
    Previously it could allow unconnected nodes that were not able to be traversed as the logic wasn't finished yet.

--- Scripts/map/gen_path_3.gd
+++ Scripts/map/gen_path_3.gd
@@ -55,13 +55,15 @@ func _gen_path_3_to(node_number,node_y_coord):
 	
 	if(node_number == 3):
 		var con_middle: int = 0
-		var middle_connection: int = 1
-		var left_cross: int = 1
-		var right_cross: int = 1
+		var middle_connection: int = 0
+		var left_cross: int = 0
+		var right_cross: int = 0
+		
 		#if 3 to 3, far left and far right have to go to each of them.
 		#the middle can go to one or both of them
 		#we are going to set the first item in the "FROM" array to the first item in the "TO" array
-	
+		
+		#we are going to set the first item in the "FROM" array to the first item in the "TO" array
 		start_local_pos.y = ((from_node_coord_array[0].y * TILE_SIZE) + TILE_SIZE)
 		start_local_pos.x = ((from_node_coord_array[0].x * TILE_SIZE) + HALF_TILE_SIZE)
 		%genPath._gen_path_line(start_local_pos,to_node_coord_array[0])
@@ -70,20 +72,37 @@ func _gen_path_3_to(node_number,node_y_coord):
 		start_local_pos.x = ((from_node_coord_array[2].x * TILE_SIZE) + HALF_TILE_SIZE)
 		%genPath._gen_path_line(start_local_pos,to_node_coord_array[2])
 		
+		#we then check if the far left will also connect to the middle, giving it a 30% chance
+		if(randi_range(1, 3) == 3):
+			start_local_pos.y = ((from_node_coord_array[0].y * TILE_SIZE) + TILE_SIZE)
+			start_local_pos.x = ((from_node_coord_array[0].x * TILE_SIZE) + HALF_TILE_SIZE)
+			%genPath._gen_path_line(start_local_pos,to_node_coord_array[1])
+			left_cross = 1
+			middle_connection = 1
+		
+		#we then check if the far right will also connect to the middle, giving it a 30% chance
+		if(randi_range(1, 3) == 3):
+			start_local_pos.y = ((from_node_coord_array[2].y * TILE_SIZE) + TILE_SIZE)
+			start_local_pos.x = ((from_node_coord_array[2].x * TILE_SIZE) + HALF_TILE_SIZE)
+			%genPath._gen_path_line(start_local_pos,to_node_coord_array[1])
+			right_cross = 1
+			middle_connection = 1
+		
 		#now we are going to take a random chance the middle node can branch to each location.
 		#it must branch to at least one of the 2.
-		if(randi_range(1, 2) == 2):
-			start_local_pos.y = ((from_node_coord_array[1].y * TILE_SIZE) + TILE_SIZE)
-			start_local_pos.x = ((from_node_coord_array[1].x * TILE_SIZE) + HALF_TILE_SIZE)
-			%genPath._gen_path_line(start_local_pos,to_node_coord_array[0])
-			con_middle = 1
-		if(randi_range(1, 2) == 2):
+		if(randi_range(1, 2) == 2 and left_cross != 1):
 			start_local_pos.y = ((from_node_coord_array[1].y * TILE_SIZE) + TILE_SIZE)
 			start_local_pos.x = ((from_node_coord_array[1].x * TILE_SIZE) + HALF_TILE_SIZE)
 			%genPath._gen_path_line(start_local_pos,to_node_coord_array[1])
-		if(con_middle != 1 or (con_middle == 1 and randi_range(1, 2) == 2)):
+			con_middle = 1
+		if(randi_range(1, 2) == 2) and right_cross != 1:
 			start_local_pos.y = ((from_node_coord_array[1].y * TILE_SIZE) + TILE_SIZE)
 			start_local_pos.x = ((from_node_coord_array[1].x * TILE_SIZE) + HALF_TILE_SIZE)
-			%genPath._gen_path_line(start_local_pos,to_node_coord_array[2])
-		con_middle = 0
-	# if 3 to 3, far sides can't go to opposite side
+			%genPath._gen_path_line(start_local_pos,to_node_coord_array[1])
+			con_middle = 1
+		#if the middle FROM node hasn't connected to the left or the right OR if the middle TO node doesn't have a connection, force the middle connection, otherwise make it random.
+		if(con_middle != 1 or middle_connection != 1 or randi_range(1, 2) == 2):
+			start_local_pos.y = ((from_node_coord_array[1].y * TILE_SIZE) + TILE_SIZE)
+			start_local_pos.x = ((from_node_coord_array[1].x * TILE_SIZE) + HALF_TILE_SIZE)
+			%genPath._gen_path_line(start_local_pos,to_node_coord_array[1])
+			
