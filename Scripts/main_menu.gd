430b0766906f87928e6756e8c6383729bae1dd64
paupertim <paupertim@gmail.com>
2026-01-28 21:12:18 -0600

    Main Menu Updates
    Menu buttons have pressed functions now.
    
    Only one setup is Quit.

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+#Play button pressed
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+#options button pressed
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+#credits button pressed
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
+
+#quit button pressed
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
