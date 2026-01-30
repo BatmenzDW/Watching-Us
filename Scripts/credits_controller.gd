22bb4ac0a5b95696bfc53fbd5c27203b7a442822
paupertim <paupertim@gmail.com>
2026-01-30 13:41:15 -0600

    Credits for newly added icons
    Added to the credits scenes for newly added icons

--- Scripts/credits_controller.gd
+++ Scripts/credits_controller.gd
@@ -29,5 +29,8 @@ func roll_credits():
 	%ThirdParty4.visible = true
 	await get_tree().create_timer(3.0).timeout
 	%ThirdParty4.visible = false
+	%ThirdParty5.visible = true
+	await get_tree().create_timer(3.0).timeout
+	%ThirdParty5.visible = false
 	%GameController.transition_state("mainmenu","")
 	%BackgroundCredits.visible = false
