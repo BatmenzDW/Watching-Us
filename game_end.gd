b3964b1ea036bbada0fb81a8d2afc6f3a62f2a47
paupertim <paupertim@gmail.com>
2026-01-30 20:41:37 -0600

    Small fix on ending
    Small fix on ending and a new image

--- game_end.gd
+++ game_end.gd
@@ -2,13 +2,12 @@ extends Node2D
 
 func end_win():
 	%EndWin.visible = true
-	await get_tree().create_timer(3.0).timeout
-	end_win2()
-
-func end_win2():
+	await get_tree().create_timer(2.0).timeout
+	%EndWin2.visible = true
+	%EndWin.visible = false
 	await get_tree().create_timer(5.0).timeout
 	%GameController.transition_state("mainmenu")
-	
+
 func end_tantrum():
 	%EndTantrum.visible = true
 	await get_tree().create_timer(5.0).timeout
